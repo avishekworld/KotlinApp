fb6f8d9f84efb2d45cc1988ddef414e9c1a122c3
Avishek Ahmed <avishek.ahmed@gmail.com>
2019-11-24 20:48:12 -0500

    kotlin app project structure + initial commit

--- app/src/main/java/life/avishekworld/kotlinapp/service/Util.kt
+++ app/src/main/java/life/avishekworld/kotlinapp/service/Util.kt
@@ -3,16 +3,9 @@ package life.avishekworld.kotlinapp.service
 import android.util.Log
 
 class Util{
-    val TAG = "Util"
 
-    fun doSomeWork(){
-        val name = "avishek"
-        log("name is  $name")
-
-        var list = 1..20
-        var evenList = list.filter { it % 2 == 0 }
-
-        evenList.forEach { x-> log(x.toString()) }
+    companion object {
+        val TAG = Util::class.java.simpleName
     }
 
     private fun log(msg:String){
